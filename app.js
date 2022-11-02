03ccb0721d0740197393c5a1e94c3dcada46f461
ignacioborraz <ignacioborraz@hotmail.com>
2022-11-02 15:08:22 -0300

    task5

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ const errorHandler = require('./src/middlewares/errorHandler')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
 app.get('/', async(_req, res) => {
     res.status(200).json({
-        env: process.env.ENVIROMENT || undefined,
+        enviroment: process.env.NODE_ENV || undefined,
         port: process.env.PORT || 8000
     })
 })
+app.use(logger('dev'))
 app.use('/api', router)
 app.use(errorHandler)
-app.use(logger('dev'))
 
 module.exports = app
