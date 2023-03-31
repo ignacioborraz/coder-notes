8da1cd070d5b843773f7ec7b3f9747de9980a975
ignacioborraz <ignacioborraz@hotmail.com>
2023-03-30 21:53:36 -0300

    clase1

--- src/daos/mongo/config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO) //MONGO es la uri de conexión
-    .then(() => console.log('DATABASE CONNECTED'))
-    .catch(err => console.log(err))
--- src/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-let errorHandler = (error,_req,res,_next) => {
-    console.log(error.stack)
-    res.status(500).json({
-        response: 'error',
-        error: error.message
-    })
-}
-
-module.exports = errorHandler
