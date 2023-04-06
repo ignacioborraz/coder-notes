92ab5384354a141d42908a00b969ffc92453fde0
ignacioborraz <ignacioborraz@hotmail.com>
2023-04-06 20:14:20 -0300

    finish

--- handsonlab/hol_02.js
+++ handsonlab/hol_02.js
@@ -27,6 +27,16 @@ class TicketManager {
         return this.events
     }
 
+    getEventById(event_id) {
+        let one = this.events.find(each=> each.id === event_id)
+        if (one) {
+            console.log(one)
+            return one
+        }
+        console.log('not found')
+        return null
+    }
+
     addEvent({ name,place,price,capacity,date }) {
         capacity = capacity ?? 50
         date = date ?? new Date()
@@ -43,12 +53,92 @@ class TicketManager {
         this.events.push(event)
     }
 
+    addParticipant(event_id,user_id) {
+        let found_event = this.getEventById(event_id)
+        //console.log(found_event.participants)
+        if (found_event) {
+            if (found_event.capacity > found_event.participants.length) {
+                let user = found_event.participants.includes(user_id)
+                if (user) {
+                    console.log(`el usuario ${user_id} ya se encuentra en la lista`)
+                } else {
+                    console.log('agregado usuario '+user_id)
+                    found_event.participants.push(user_id)
+                    //verificar que el array this.events fue o no fue modificado
+                    //en caso de no modificarse: usar un for para cambiar el evento con la modificacion realizada
+                    //console.log(found_event.participants)
+                    //return user_id        //lo sacamos de la condicion ya que es necesario seguir con la condicion del cambio d enombre
+                }
+            } else {
+                console.log('no hay mas capacidad')
+            }
+        }/*  else {
+            console.log('no se encontró el evento '+event_id);
+        } */
+        return null
+    }
+
+    changeName(event_id,new_name) {
+        let found_event = this.getEventById(event_id)
+        found_event.name = new_name
+            /*
+            for (let each of this.events) {
+                if (each.id === event_id) { //si coincide el id del evento a modificar
+                    return found_event      //retorno el evento encontrado y luego modificado
+                } else {                    //en caso contrario (no coincide)
+                    return each             //retorno el evento del array
+                }
+            }
+            */
+        console.log('nombre cambiado')
+    }
+
+    editEvent(event_id,datos) {  //datos va a ser el objeto con las variables a modificar (puede ser solo name o puede name con place o pueden ser TODOS los campos)
+        let found_event = this.getEventById(event_id)
+        for (let propiedad in datos) {
+            //console.log(propiedad)
+            found_event[propiedad] = datos[propiedad]
+        }
+    }
+
+    addNewEvent(event_id,new_place,new_date) {
+        let found_event = { ...this.getEventById(event_id) }
+        this.addEvent({
+            name: found_event.name,
+            place: new_place,
+            price: found_event.price,
+            capacity: found_event.capacity,
+            date: new_date
+        })
+        console.log('se creo el nuevo evento')
+    }
+
+    deleteEvent(event_id) {
+        this.events = this.events.filter(each => each.id !== event_id)
+        console.log('evento eliminado')
+    }
+
 }
 
 let ticket = new TicketManager()
 //console.log(ticket.gain)
 ticket.addEvent({ name: 'alice in borderland', place: 'korea', price: 5,capacity: null, date: undefined })
 ticket.addEvent({ name: 'hp', place: 'england', price: 10 })
-ticket.addEvent({ name: 'pokemon', place: 'japon', price: 50,capacity: 100000, date: new Date('07/09/2023') })
+ticket.addEvent({ name: 'pokemon', place: 'japon', price: 50,capacity: 2, date: new Date('07/09/2023') })
 ticket.addEvent({ name: 'disney', place: 'miami', price: 100,capacity: 5000, date: new Date('07/20/2023') })
-ticket.getEvents()
+//ticket.getEvents()
+//ticket.getEventById(2)
+//ticket.getEventById(9)
+//ticket.addParticipant(3,5)
+//ticket.addParticipant(3,9)
+//ticket.addParticipant(3,15)
+//ticket.addParticipant(1,5)
+//ticket.addParticipant(1,5)
+//ticket.addParticipant(1,5,'hola')
+//ticket.getEventById(1)
+//ticket.addNewEvent(3,'china',new Date('08/20/2023'))
+//ticket.getEvents()
+//ticket.deleteEvent(1)
+//ticket.getEvents()
+ticket.editEvent(1,{name: 'hola', place: 'rosario'})
+ticket.getEventById(1)
