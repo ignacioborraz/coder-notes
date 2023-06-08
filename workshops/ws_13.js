ba934959f546aa8773deeefdad095dcc7daa5905
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-08 16:54:39 -0300

    fix

---
+++ workshops/ws_13.js
@@ -0,0 +1,13 @@
+let alumnos = [
+    { nombre: "alan", apellido: "jimenez", curso: "back", edad: 30, correo: "mail@mail.com", sexo: "M" },
+    { nombre: "igna", apellido: "borraz", curso: "data", edad: 32, correo: "mail@mail.com", sexo: "M" },
+    { nombre: "ursula", apellido: "aguirre", curso: "full", edad: 35, correo: "mail@mail.com", sexo: "F" },
+    { nombre: "guille", apellido: "perez", curso: "full", edad: 38, correo: "mail@mail.com", sexo: "M" },
+    { nombre: "nico", apellido: "lopez", curso: "front", edad: 25, correo: "mail@mail.com", sexo: "M" },
+    { nombre: "zoe", apellido: "quintana", curso: "full", edad: 20, correo: "mail@mail.com", sexo: "F" },
+    { nombre: "barbara", apellido: "colombo", curso: "full", edad: 27, correo: "mail@mail.com", sexo: "F" },
+    { nombre: "flor", apellido: "encia", curso: "front", edad: 29, correo: "mail@mail.com", sexo: "F" },
+    { nombre: "kevin", apellido: "hitos", curso: "back", edad: 31, correo: "mail@mail.com", sexo: "M" },
+    { nombre: "matias", apellido: "giordano", curso: "data", edad: 18, correo: "mail@mail.com", sexo: "M" },
+    { nombre: "socorro", apellido: "emesha", curso: "back", edad: 22, correo: "mail@mail.com", sexo: "F" },
+]
