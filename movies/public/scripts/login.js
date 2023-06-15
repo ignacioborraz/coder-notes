55ce1e9d4c5e34bed969aec299470dc4e82625fb
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-15 15:48:00 -0300

    files

---
+++ movies/public/scripts/login.js
@@ -0,0 +1,16 @@
+document.getElementById('login').addEventListener('click',(event)=>{
+    event.preventDefault()
+    let data = {
+        mail: document.querySelector('#mail').value,
+        password: document.querySelector('#password').value,
+    }
+    console.log(data)
+    fetch(`/api/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+        .then(res=>res.json())
+        .then(res=>console.log(res))    //en lugar de imprimir en consola: mostrar mensaje de alerta
+        .catch(err=>console.log(err))   //en lugar de imprimir en consola: mostrar mensaje de alerta
+})
