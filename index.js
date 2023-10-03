de6238b6c113f187222fd41f9c885d574cd8ba40
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-03 20:07:48 -0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const sumar = (n1, n2) => {
     n1 = Number(n1)
     n2 = Number(n2)
-    n1 + n2;
+    return n1 + n2;
 }
 const restar = (n1, n2) => Number(n1) - Number(n2);
 const multiplicar = (n1, n2) => n1 * n2;
