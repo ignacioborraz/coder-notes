88b576159bc2417236e9b99cd1a12bbf356eb2b5
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-03 18:28:23 -0300

    fix

--- sum.js
+++ sum.js
@@ -1,3 +1,6 @@
 const sumar = (num1, num2) => num1 + num2;
+const restar = (num1, num2) => num1 - num2;
+const dividir = (num1, num2) => num1 / num2;
+const multiplicar = (num1, num2) => num1 * num2;
 
-export default { sumar };
+export default { sumar,restar,dividir,multiplicar };
