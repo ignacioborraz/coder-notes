e7e1a348dc21bd7f1348dfff94dca06f2df1f852
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-03 19:51:48 -0300

    calculator

---
+++ index.js
@@ -0,0 +1,6 @@
+const sumar = (n1, n2) => n1 + n2;
+const restar = (n1, n2) => n1 - n2;
+const multiplicar = (n1, n2) => n1 * n2;
+const dividir = (n1, n2) => n1 / n2;
+
+export default { sumar, restar, multiplicar, dividir };
